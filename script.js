908c6b7f2c204de93fe34bbd019ec335001372f3
maha-abdelzaher <maha.abdelzaher@gmail.com>
2022-07-22 22:09:25 +0200

    mobile view

--- script.js
+++ script.js
@@ -81,7 +81,7 @@ btnHold.addEventListener('click', function () {
       scores[activePlayer];
 
     // 2- check if player's score is >= 100
-    if (scores[activePlayer] >= 20) {
+    if (scores[activePlayer] >= 100) {
       // finish the game
       playing = false;
       document
